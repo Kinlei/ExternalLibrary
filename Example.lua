a996b9571c08d0c4e4db9b45c9ef381acff360f3
Kinlei <61442909+Kinlei@users.noreply.github.com>
2020-08-29 08:23:20 +0100

    Create Example.lua

---
+++ Example.lua
@@ -0,0 +1,7 @@
+local UI = loadstring(game:HttpGet'https://raw.githubusercontent.com/flareo/external-UI-Library/master/Source.lua')() -- Requests API
+
+local Tab = UI.createCategory('Tab Name') -- Creates tab/category
+
+Tab:addToggle('Toggle', Callback) -- Boolean toggle with callback
+Tab:addSlider('Slider', Callback, minimumValue, maximumValue, showDecimals, roundToNthDecimal) -- Slider with callback with minimum and maximum values
+Tab:addButton('Button', Callback) -- Button with callback
